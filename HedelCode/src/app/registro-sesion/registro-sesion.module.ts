0278c403083e477cb95241caf33efd711d73efd1
henry10110 <henrygironnavas@gmail.com>
2023-04-14 19:34:54 -0600

    diseño minimo de registro e inicio de sesion completado

--- HedelCode/src/app/registro-sesion/registro-sesion.module.ts
+++ HedelCode/src/app/registro-sesion/registro-sesion.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistroComponent } from './registro/registro.component';
+import { SesionComponent } from './sesion/sesion.component';
 
 
 
 @NgModule({
-  declarations: [],
-  exports: [],
+  declarations: [
+    RegistroComponent,
+    SesionComponent
+  ],
+  exports: [
+    RegistroComponent,
+    SesionComponent
+  ],
   imports: [
     CommonModule
   ],
